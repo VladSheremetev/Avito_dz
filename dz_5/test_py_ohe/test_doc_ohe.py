441f26c65c1e13ed0ce26eae721b6e77c5b5a148
Vladislav Sheremetev <68332331+VladSheremetev@users.noreply.github.com>
2021-11-28 14:43:06 +0300

    Create test_doc_ohe.py

---
+++ dz_5/test_py_ohe/test_doc_ohe.py
@@ -0,0 +1,76 @@
+import pytest
+from typing import List, Tuple
+
+
+def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
+    """
+    fit_transform(iterable)
+    fit_transform(arg1, arg2, *args)
+    """
+    if len(args) == 0:
+        raise TypeError('expected at least 1 arguments, got 0')
+
+    categories = args if isinstance(args[0], str) else list(args[0])
+    uniq_categories = set(categories)
+    bin_format = f'{{0:0{len(uniq_categories)}b}}'
+
+    seen_categories = dict()
+    transformed_rows = []
+
+    for cat in categories:
+        bin_view_cat = (int(b) for b in bin_format.format(1 << len(seen_categories)))
+        seen_categories.setdefault(cat, list(bin_view_cat))
+        transformed_rows.append((cat, seen_categories[cat]))
+
+    return transformed_rows
+
+
+def test_fit_transform_0():
+    data = []
+    exp_transformed = []
+    assert fit_transform(data) == exp_transformed
+
+
+def test_fit_transform_1():
+    data = ['Moscow', 'New York', 'Moscow', 'London']
+    exp_transformed = [
+        ('Moscow', [0, 0, 1]),
+        ('New York', [0, 1, 0]),
+        ('Moscow', [0, 0, 1]),
+        ('London', [1, 0, 0]),
+    ]
+    assert fit_transform(data) == exp_transformed
+
+
+def test_fit_transform_2():
+    data = [1]
+    exp_transformed = [
+        (1, [1]),
+    ]
+    assert fit_transform(data) == exp_transformed
+
+
+def test_fit_transform_3():
+    data = [1, '', ['c']]
+    with pytest.raises(TypeError):
+        fit_transform(data)
+
+
+def test_fit_transform_error():
+    with pytest.raises(TypeError):
+        fit_transform()
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    cities = ['Moscow', 'New York', 'Moscow', 'London']
+    exp_transformed_cities = [
+        ('Moscow', [0, 0, 1]),
+        ('New York', [0, 1, 0]),
+        ('Moscow', [0, 0, 1]),
+        ('London', [1, 0, 0]),
+    ]
+    transformed_cities = fit_transform(cities)
+    pprint(transformed_cities)
+    assert transformed_cities == exp_transformed_cities
