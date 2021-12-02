bef69bc5f06cccd8bd74284584d932f6ebe1156f
Vladislav Sheremetev <68332331+VladSheremetev@users.noreply.github.com>
2021-12-02 22:55:28 +0300

    Update test_what_is_year_now.py

--- dz_5/test_mock/test_what_is_year_now.py
+++ dz_5/test_mock/test_what_is_year_now.py
@@ -1,30 +1,33 @@
-from wyn import What_is_year_now
+from wyn import WhatIsYearNow
 from unittest.mock import patch
+import io
+import urllib.request
 import unittest
 
 
 class TestWY(unittest.TestCase):
 
-    @patch('wyn.What_is_year_now.get_data', return_value='01.03.2021')
+    @patch('wyn.WhatIsYearNow.get_data', return_value={"currentDateTime": "01.03.2021"})
     def test_what_is_year_now_0(self, func):
-        self.assertEqual(What_is_year_now().what_is_year_now(), 2021)
+        self.assertEqual(WhatIsYearNow().what_is_year_now(), 2021)
 
-    @patch('wyn.What_is_year_now.get_data', return_value='01.03/2021')
+    @patch('wyn.WhatIsYearNow.get_data', return_value={"currentDateTime": "01.03/2021"})
     def test_what_is_year_now_1(self, func):
         with self.assertRaises(ValueError):
-            self.assertRaises(What_is_year_now().what_is_year_now())
+            self.assertRaises(WhatIsYearNow().what_is_year_now())
 
-    @patch('wyn.What_is_year_now.get_data', return_value='2021-03-01')
+    @patch('wyn.WhatIsYearNow.get_data', return_value={"currentDateTime": "2021-03-01"})
     def test_what_is_year_now_2(self, func):
-        self.assertEqual(What_is_year_now().what_is_year_now(), 2021)
+        self.assertEqual(WhatIsYearNow().what_is_year_now(), 2021)
 
-    @patch('wyn.What_is_year_now.get_data', return_value='2021-03-01')
+    @patch('wyn.WhatIsYearNow.get_data', return_value={"currentDateTime": "2021-03-01"})
     def test_what_is_year_now_3(self, func):
-        self.assertIsInstance(What_is_year_now().what_is_year_now(), int)
+        self.assertIsInstance(WhatIsYearNow().what_is_year_now(), int)
 
     def test_what_is_year_now_4(self):
-        date = What_is_year_now().get_data()
-        self.assertEqual(date[:10], '2021-11-29')
+        date = io.StringIO('{"currentDateTime": "2021-03-01"}')
+        with patch.object(urllib.request, 'urlopen', return_value=date):
+            self.assertEqual(WhatIsYearNow().get_data(), {"currentDateTime": "2021-03-01"})
 
 
 if __name__ == '__main__':
